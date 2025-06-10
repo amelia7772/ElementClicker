28af864ff798ca745cfee7035e91f4d6d5248b7c
amelia7772 <ameliashananya@gmail.com>
2025-06-10 14:40:26 +0300

    Delete CraftingRecipe.py

--- CraftingRecipe.py
+++
@@ -1,9 +0,0 @@
-from ElementType import ElementType
-
-class CraftingRecipe:
-    def __init__(self, *ingredients: tuple[ElementType, int, bool], result: tuple[ElementType, int], resulting_xp: int, waiting_time: float):
-        self.ingredients = ingredients
-        self.result = result
-        self.resulting_xp = resulting_xp
-        self.waiting_time = waiting_time
-        self.original_waiting_time = waiting_time
