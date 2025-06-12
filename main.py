b5a951b68a7e3affafe542ad6dea64886dd62dd8
amelia7772 <ameliashananya@gmail.com>
2025-06-12 20:14:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,12 +29,41 @@ quest_scene = QuestScene(background_image)
 while True:
     dt = time.time() - previous_time
     previous_time = time.time()
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            save_manager.save_game()
+            exit()
+        elif event.type == pygame.VIDEORESIZE and not pygame.display.is_fullscreen():
+            main_scene.previous_size = main_scene.screen_size
+            quest_scene.previous_size = quest_scene.screen_size
+            if event.size[0] >= 800 and event.size[1] >= 400:
+                Screen.screen = pygame.display.set_mode(event.size, pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE)
+            else:
+                Screen.screen = pygame.display.set_mode((800,400), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE)
+            main_scene.resize_scene(Screen.screen.get_size())
+            quest_scene.resize_scene(Screen.screen.get_size())
+                
+    if pygame.key.get_pressed()[pygame.key.key_code("f11")]:
+        if not pygame.display.is_fullscreen():
+            main_scene.previous_size = Screen.screen.get_size()
+            quest_scene.previous_size = Screen.screen.get_size()
+            Screen.screen = pygame.display.set_mode(pygame.display.list_modes()[0])
+            Screen.screen = pygame.display.set_mode(pygame.display.list_modes()[0], pygame.FULLSCREEN)
+        else:
+            Screen.screen = pygame.display.set_mode(main_scene.previous_size, pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE)
+            main_scene.previous_size = Screen.monitor_size
+            quest_scene.previous_size = Screen.monitor_size
+        main_scene.resize_scene(Screen.screen.get_size())
+        quest_scene.resize_scene(Screen.screen.get_size())
+        
     if active_scene == Scene.main:
-        main_scene.update(dt, Screen.monitor_size)
+        main_scene.update(dt, events)
         active_scene = main_scene.get_active_scene()
         quest_scene.set_active_scene(active_scene)
     elif active_scene == Scene.quest_scene:
-        quest_scene.update(Screen.monitor_size)
+        quest_scene.update(events)
         active_scene = quest_scene.get_active_scene()
         main_scene.set_active_scene(active_scene)
     pygame.display.update()
