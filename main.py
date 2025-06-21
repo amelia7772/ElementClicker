bfca8d522b46e2b73f4ed6bfde5da464067626e6
amelia7772 <ameliashananya@gmail.com>
2025-06-21 07:40:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from utilities.scene import *
 from main_game_screen.MainScene import *
 from quest_screen.QuestScene import *
 from utilities.SaveManager import *
+from quest_screen.QuestLine import *
 
 Screen.screen.fill((194, 255, 250))
 
@@ -27,6 +28,33 @@ main_scene = MainScene(background_image)
 quest_scene = QuestScene(background_image)
 
 timer_for_saving_game = 0.0
+timer_for_factory_tier_one = 0.0
+
+#elements.elements[int(ElementType.factory_tier_one)].element_resource_amount += 1
+
+def evaluate_crafting_timers():
+    counter = 0
+    for crafting_timer in crafting_timers:
+        recipe = get_recipe_for(ElementType(counter))
+        if crafting_timer >= 0:
+            if (time.time() - crafting_timer) >= recipe.waiting_time:
+                if len(recipe.ingredients[0]) != 0:
+                    for ingredient in recipe.ingredients:
+                        if ingredient[2]:
+                            elements.elements[int(ingredient[0])].increase_element_amount(-ingredient[1], Screen.screen)
+                elements.elements[int(recipe.result[0])].increase_element_amount(recipe.result[1], Screen.screen)
+                reevaluate_recipes_waiting_time()
+                xp_bar.increase_xp(recipe.resulting_xp, Screen.screen)
+                crafting_timers[counter] = -1
+                elements.elements[int(recipe.result[0])].set_element_is_pressed(False)
+                elements.elements[int(recipe.result[0])]._is_element_currently_being_crafted = False
+                elements.elements[int(recipe.result[0])]._crafting_prorgress = 0.0
+                for quest in quests:
+                    if (quest.condition(elements.elements, xp_bar.level)) and (not quest.is_completed):
+                        quest_line.set_quest_completed(quest.id, True)
+            else:
+                elements.elements[int(recipe.result[0])]._crafting_prorgress = (time.time() - crafting_timer) / float(recipe.waiting_time)
+        counter += 1
 
 while True:
     dt = time.time() - previous_time
@@ -63,7 +91,8 @@ while True:
             quest_scene.previous_size = Screen.monitor_size
         main_scene.resize_scene(Screen.screen.get_size())
         quest_scene.resize_scene(Screen.screen.get_size())
-        
+    
+    evaluate_crafting_timers()
     if active_scene == Scene.main:
         main_scene.update(dt, events)
         active_scene = main_scene.get_active_scene()
