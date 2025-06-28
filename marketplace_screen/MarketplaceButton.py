7aa714e490e104d69e2058b8ec82c82213e1e6fd
amelia7772 <ameliashananya@gmail.com>
2025-06-28 22:15:39 +0300

    Add files via upload

---
+++ marketplace_screen/MarketplaceButton.py
@@ -0,0 +1,13 @@
+import pygame
+from utilities.UiElement import *
+
+class MarketplaceButton(UiElement):
+    def __init__(self, background: pygame.Surface, icon_image: pygame.Surface):
+        super().__init__([background, icon_image], [(65, 65), (50, 50)], True)
+    
+    def draw(self, screen: pygame.Surface):
+        background_x = 0
+        background_y = screen.get_height() - self.images[0].get_height()
+        icon_x = background_x + ((self.images[0].get_width() / 2) - (self.images[1].get_width() / 2))
+        icon_y = background_y + ((self.images[0].get_height() / 2) - (self.images[1].get_height() / 2))
+        super().draw(screen, [(background_x, background_y), (icon_x, icon_y)])
---
+++ marketplace_screen/MarketplaceScene.py
@@ -0,0 +1,47 @@
+import pygame
+import os
+from utilities import Screen
+from utilities.Scene import Scene
+from quest_screen.QuestButton import QuestButton
+from marketplace_screen.MarketplaceButton import MarketplaceButton
+
+class MarketplaceScene:
+    
+    def __init__(self, background_image: pygame.Surface):
+        self.background_image = background_image.copy()
+        
+        self.quest_button = QuestButton(pygame.image.load(os.path.join("assets", "images" ,"quest button background.png")).convert_alpha(), pygame.image.load(os.path.join("assets", "images" ,"quest button icon.png")).convert_alpha())
+        self.marketplace_button = MarketplaceButton(pygame.image.load(os.path.join("assets", "images" ,"quest button background.png")).convert_alpha(), pygame.image.load(os.path.join("assets", "images" ,"marketplace button icon.png")).convert_alpha())
+        
+        self.screen_size = Screen.screen.get_size()
+        self.previous_size = self.screen_size
+        
+        self.active_scene = Scene.main
+    
+    def update(self, dt, events):
+        Screen.screen.fill((46, 46, 46))
+        
+        for event in events:
+            if event.type == pygame.MOUSEMOTION:
+                mouse_position = pygame.mouse.get_pos()
+                self.quest_button.is_highlighted = self.quest_button._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
+                                    
+            elif event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+                if self.quest_button.is_highlighted and not self.quest_button.is_ui_element_pressed():
+                    self.quest_button.set_ui_element_is_pressed(True)
+            
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if self.quest_button.is_ui_element_pressed():
+                    self.quest_button.set_ui_element_is_pressed(False)
+                    self.active_scene = Scene.main
+        
+        for x in range(0, Screen.screen.get_width(), self.background_image.get_width()):
+            for y in range(0, Screen.screen.get_height(), self.background_image.get_height()):
+                Screen.screen.blit(self.background_image, (x,y))
+    
+    def set_active_scene(self, active_scene: Scene):
+        self.active_scene = active_scene
+        
+    
+    def get_active_scene(self):
+        return self.active_scene
