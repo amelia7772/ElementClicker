b51a5aeba1891412ca911df317c3ead5087dc968
amelia7772 <ameliashananya@gmail.com>
2025-07-02 19:52:00 +0300

    Update Scene.py

--- utilities/Scene.py
+++ utilities/Scene.py
@@ -3,4 +3,6 @@ from enum import IntEnum
 class Scene(IntEnum):
     main = 0,
     quest_scene = 1,
-    marketplace_scene = 2
+    marketplace_scene = 2,
+    settings_scene = 3,
+    credits_scene = 4
