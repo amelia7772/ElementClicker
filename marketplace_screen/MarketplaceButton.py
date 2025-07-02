926e0b19368794b9a11864fc6934d29c84744d54
amelia7772 <ameliashananya@gmail.com>
2025-07-02 20:29:51 +0300

    Update MarketplaceButton.py

--- marketplace_screen/MarketplaceButton.py
+++ marketplace_screen/MarketplaceButton.py
@@ -3,7 +3,7 @@ from utilities.UiElement import *
 
 class MarketplaceButton(UiElement):
     def __init__(self, background: pygame.Surface, icon_image: pygame.Surface):
-        super().__init__([background, icon_image], [(65, 65), (50, 50)], True)
+        super().__init__([background, icon_image], [(75, 75), (50, 50)], True)
     
     def draw(self, screen: pygame.Surface):
         background_x = 0
