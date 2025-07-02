979c55068c8697b9b56c2b756bf430efac02b1ee
amelia7772 <ameliashananya@gmail.com>
2025-07-02 20:44:42 +0300

    Update MarketplaceScene.py

--- marketplace_screen/MarketplaceScene.py
+++ marketplace_screen/MarketplaceScene.py
@@ -4,6 +4,8 @@ from utilities import Screen
 from utilities.Scene import Scene
 from quest_screen.QuestButton import QuestButton
 from marketplace_screen.MarketplaceButton import MarketplaceButton
+from settings_screen.SettingsButton import SettingsButton
+from credits_screen.CreditsButton import CreditsButton
 from utilities.UiElement import UiElement
 from marketplace_screen import Money
 from utilities.BigNumberMap import order_of_magnitude_to_symbol_map
@@ -11,7 +13,6 @@ from marketplace_screen.GoodsLine import GoodsLine
 from main_game_screen.ElementType import ElementType
 from main_game_screen.Elements import elements
 from math import floor
-import math
 
 class MarketplaceScene:
     
@@ -30,6 +31,8 @@ class MarketplaceScene:
         
         self.quest_button = QuestButton(pygame.image.load(os.path.join("assets", "images" ,"quest button background.png")).convert_alpha(), pygame.image.load(os.path.join("assets", "images" ,"quest button icon.png")).convert_alpha())
         self.marketplace_button = MarketplaceButton(pygame.image.load(os.path.join("assets", "images" ,"quest button background.png")).convert_alpha(), pygame.image.load(os.path.join("assets", "images" ,"marketplace button icon.png")).convert_alpha())
+        self.setting_buttons = SettingsButton(pygame.image.load(os.path.join("assets", "images" ,"quest button background.png")).convert_alpha(), pygame.image.load(os.path.join("assets", "images" ,"settings button icon.png")).convert_alpha())
+        self.credits_buttons = CreditsButton(pygame.image.load(os.path.join("assets", "images" ,"quest button background.png")).convert_alpha(), pygame.image.load(os.path.join("assets", "images" ,"credits button icon.png")).convert_alpha())
         
         self.scroll_offset = 0.0
         
@@ -184,6 +187,8 @@ class MarketplaceScene:
                 mouse_position = pygame.mouse.get_pos()
                 self.quest_button.is_highlighted = self.quest_button._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
                 self.marketplace_button.is_highlighted = self.marketplace_button._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
+                self.setting_buttons.is_highlighted = self.setting_buttons._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
+                self.credits_buttons.is_highlighted = self.credits_buttons._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
                 self.amount_per_transaction_button.is_highlighted = self.amount_per_transaction_button._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
                 self.type_of_transaction_button.is_highlighted = self.type_of_transaction_button._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
 
@@ -198,6 +203,10 @@ class MarketplaceScene:
                     self.quest_button.set_ui_element_is_pressed(True)
                 if self.marketplace_button.is_highlighted and not self.marketplace_button.is_ui_element_pressed():
                     self.marketplace_button.set_ui_element_is_pressed(True)
+                if self.setting_buttons.is_highlighted and not self.setting_buttons.is_ui_element_pressed():
+                    self.setting_buttons.set_ui_element_is_pressed(True)
+                if self.credits_buttons.is_highlighted and not self.credits_buttons.is_ui_element_pressed():
+                    self.credits_buttons.set_ui_element_is_pressed(True)
                 if self.amount_per_transaction_button.is_highlighted and not self.amount_per_transaction_button.is_ui_element_pressed():
                     self.amount_per_transaction_button.set_ui_element_is_pressed(True)
                 if self.type_of_transaction_button.is_highlighted and not self.type_of_transaction_button.is_ui_element_pressed():
@@ -214,6 +223,12 @@ class MarketplaceScene:
                 if self.marketplace_button.is_ui_element_pressed():
                     self.marketplace_button.set_ui_element_is_pressed(False)
                     self.active_scene = Scene.main
+                if self.setting_buttons.is_ui_element_pressed():
+                    self.setting_buttons.set_ui_element_is_pressed(False)
+                    self.active_scene = Scene.settings_scene
+                if self.credits_buttons.is_ui_element_pressed():
+                    self.credits_buttons.set_ui_element_is_pressed(False)
+                    self.active_scene = Scene.credits_scene
                 if self.amount_per_transaction_button.is_ui_element_pressed():
                     self.amount_per_transaction_button.set_ui_element_is_pressed(False)
                     if self.amount_per_transaction_option < 3:
@@ -261,6 +276,8 @@ class MarketplaceScene:
         
         self.quest_button.draw(Screen.screen)
         self.marketplace_button.draw(Screen.screen)
+        self.setting_buttons.draw(Screen.screen)
+        self.credits_buttons.draw(Screen.screen)
         
         Screen.screen.blit(self.shadow_bounding_box_surface, self.shadow_bounding_box_rect)
         self.money_amount_text.draw(Screen.screen, [self.money_amount_text_rect.topleft])
@@ -279,6 +296,8 @@ class MarketplaceScene:
             mouse_position = pygame.mouse.get_pos()
             self.quest_button.is_highlighted = self.quest_button._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
             self.marketplace_button.is_highlighted = self.marketplace_button._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
+            self.setting_buttons.is_highlighted = self.setting_buttons._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
+            self.credits_buttons.is_highlighted = self.credits_buttons._hightliter_ellipse.collide_point(float(mouse_position[0]), float(mouse_position[1]))
             self.active_scene = active_scene
             
     def get_active_scene(self):
@@ -337,7 +356,9 @@ class MarketplaceScene:
     def resize_scene(self, new_size: tuple[int,int]):
         self.quest_button.resize_ui_element(float(new_size[0]) / float(self.previous_size[0]), float(new_size[1]) / float(self.previous_size[1]))
         self.marketplace_button.resize_ui_element(float(new_size[0]) / float(self.previous_size[0]), float(new_size[1]) / float(self.previous_size[1]))
-
+        self.setting_buttons.resize_ui_element(float(new_size[0]) / float(self.previous_size[0]), float(new_size[1]) / float(self.previous_size[1]))
+        self.credits_buttons.resize_ui_element(float(new_size[0]) / float(self.previous_size[0]), float(new_size[1]) / float(self.previous_size[1]))
+        
         self.shadow_bounding_box_surface = pygame.Surface((int(float(Screen.screen.get_width()) * 0.6), Screen.screen.get_height() * 2),pygame.SRCALPHA)
         
         self.shadow_bounding_box_surface.fill((0,0,0,int(0.4 * 255)))
