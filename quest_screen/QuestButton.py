917a402f959cf3ef53e86925a3115802bda0da6d
amelia7772 <ameliashananya@gmail.com>
2025-07-03 11:40:58 +0300

    Update QuestButton.py

--- quest_screen/QuestButton.py
+++ quest_screen/QuestButton.py
@@ -1,10 +1,14 @@
 import pygame
 from utilities.UiElement import *
+from utilities import Screen
 
 class QuestButton(UiElement):
     def __init__(self, background: pygame.Surface, icon_image: pygame.Surface):
         super().__init__([background, icon_image], [(65, 65), (50, 50)], True)
-    
+        self._hightliter_ellipse.topleft = (Screen.screen.get_width() - self.images[0].get_width(), Screen.screen.get_height() - self.images[0].get_height())
+        self._hightliter_ellipse.float_top = float(self._hightliter_ellipse.top)
+        self._hightliter_ellipse.float_left = float(self._hightliter_ellipse.left)
+        
     def draw(self, screen: pygame.Surface):
         background_x = screen.get_width() - self.images[0].get_width()
         background_y = screen.get_height() - self.images[0].get_height()
