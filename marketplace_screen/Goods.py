01a165adcfcfcefb7cdbf0407875efa74b914b5a
amelia7772 <ameliashananya@gmail.com>
2025-07-07 05:13:36 +0300

    Update Goods.py

--- marketplace_screen/Goods.py
+++ marketplace_screen/Goods.py
@@ -2,4 +2,10 @@ from main_game_screen.ElementType import ElementType
 
 global goods
 #element_id, price_buy, price_sell, is_available, level_requirement, element_unlocking_requirement (list of elements that should be unlocked and their amounts)
-goods: list[tuple[ElementType, float, float, bool, int, list[tuple[ElementType, int]]]] = [(ElementType.wood, 1, 1, False, 0, []), (ElementType.rock, 2, 1, False, 5, [(ElementType.rock, 1)]), (ElementType.water, 10, 3, False, 7, [(ElementType.bucket, 1)]), (ElementType.dirt, 10, 3, False, 10, [(ElementType.bucket, 1)])]
+goods: list[tuple[ElementType, float, float, bool, int, list[tuple[ElementType, int]]]] = \
+[\
+(ElementType.wood, 1, 1, False, 0, []),\
+(ElementType.rock, 2, 1, False, 5, [(ElementType.rock, 1)]), \
+(ElementType.water, 10, 3, False, 7, [(ElementType.bucket, 1)]), \
+(ElementType.dirt, 10, 3, False, 10, [(ElementType.iron_shovel, 1)])\
+]
