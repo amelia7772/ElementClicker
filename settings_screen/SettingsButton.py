ea3257b38f68b696993dbb8701359d469a4d8ef6
amelia7772 <ameliashananya@gmail.com>
2025-07-08 00:54:32 +0300

    Update SettingsButton.py

--- settings_screen/SettingsButton.py
+++ settings_screen/SettingsButton.py
@@ -4,7 +4,9 @@ from utilities import Screen
 
 class SettingsButton(UiElement):
     def __init__(self, background: pygame.Surface, icon_image: pygame.Surface):
-        super().__init__([background, icon_image], [(75, 75), (50, 50)], True)
+        background_resized = pygame.transform.scale(background, (96, 96))
+        #icon_image_resized = pygame.transform.scale(icon_image, (64, 64))
+        super().__init__([background_resized, icon_image], [(96, 96), (64, 64)], True)
         self._hightliter_ellipse.topleft = (Screen.screen.get_width() - self.images[0].get_width(), 0)
         self._hightliter_ellipse.float_top = float(self._hightliter_ellipse.top)
         self._hightliter_ellipse.float_left = float(self._hightliter_ellipse.left)
