5a987e28d981b39709292a130fbe2b3a9a505f41
amelia7772 <ameliashananya@gmail.com>
2025-07-08 14:44:29 +0300

    Update Events.py

--- utilities/Events.py
+++ utilities/Events.py
@@ -1,7 +1,8 @@
 from enum import IntEnum
 
 class Event(IntEnum):
-    update_marketplace_goods_availability = 1
+    update_marketplace_goods_availability = 1,
+    money_amount_decrease = 2
 
 global game_events
 game_events: list[Event] = []
