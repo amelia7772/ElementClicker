6f9a6cb9cfee93c2bb813eef6acf2586bb1e10f0
amelia7772 <ameliashananya@gmail.com>
2025-08-10 22:34:34 +0300

    Update Screen.py

--- utilities/Screen.py
+++ utilities/Screen.py
@@ -4,7 +4,7 @@ import os
 global monitor_size
 monitor_size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 global screen
-screen = pygame.display.set_mode((800, 400), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE)
+screen = pygame.display.set_mode((800, 400), pygame.HWSURFACE|pygame.DOUBLEBUF|pygame.RESIZABLE, vsync=1)
 pygame.display.set_caption("Element Clicker")
 logo_image = pygame.image.load(os.path.join("assets", "images" ,"icon.png")).convert_alpha()
 pygame.display.set_icon(logo_image)
